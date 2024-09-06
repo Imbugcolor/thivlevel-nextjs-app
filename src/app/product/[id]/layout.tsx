f2b3cf5fc4eb745950f9755162982d0a9b241ef5
Imbugcolor <viethd123456@gmail.com>
2024-09-06 20:42:56 +0700

    metadata props

--- src/app/product/[id]/layout.tsx
+++ src/app/product/[id]/layout.tsx
@@ -3,11 +3,10 @@ import type { Metadata, ResolvingMetadata } from "next";
 
 type Props = {
     params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export async function generateMetadata(
-    { params, searchParams }: Props,
+    { params }: Props,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
     // read route params
