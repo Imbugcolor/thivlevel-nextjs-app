0902f6cc61c5f7e06583825d18637dd72a80afb2
Imbugcolor <viethd123456@gmail.com>
2024-09-07 10:10:58 +0700

    production url config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const NEXT_SERVER_URL = 'http://localhost:10000';
+export const NEXT_SERVER_URL = 'https://thivlevel-nextjs-app.onrender.com';
 export const BACKEND_SERVER_URL = process.env.NEXT_PUBLIC_API_ENDPOINT;
 export const NUM_PER_PAGE = 8;
