a63c8d1e4c206482db0b98a53341125c8897161d
Imbugcolor <viethd123456@gmail.com>
2024-09-28 18:45:42 +0700

    URL backend config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-// export const NEXT_SERVER_URL = 'https://thivlevel-nextjs-app.onrender.com';
-export const NEXT_SERVER_URL = 'http://localhost:8080';
+export const NEXT_SERVER_URL = 'https://thivlevel-nextjs-app.onrender.com';
+// export const NEXT_SERVER_URL = 'http://localhost:8080';
 export const BACKEND_SERVER_URL = process.env.NEXT_PUBLIC_API_ENDPOINT;
 export const NUM_PER_PAGE = 10;
